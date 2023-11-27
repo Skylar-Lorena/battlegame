ea3b18162588aaa4ee55af64a066d032ad57a866
Skylar-Lorena <lorayne911@gmail.com>
2023-11-27 14:50:52 +0300

    task1 : Set game variables: characters & stats

---
+++ battlegame.py
@@ -0,0 +1,13 @@
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+
+wizard_hp = 70
+elf_hp = 90
+human_hp = 88
+dragon_hp = 300
+
+wizard_damage = 150
+elf_damage = 90
+human_damage = 99
+dragon_damage = 50
