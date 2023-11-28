796f96a3fb987e87652c72c3a6c8d34e45e38482
Skylar-Lorena <lorayne911@gmail.com>
2023-11-28 14:51:22 +0300

    loop to congratulate player for defeating dragon

--- battlegame.py
+++ battlegame.py
@@ -47,7 +47,7 @@ while True:
                     "The dragon has defeated you!"
                 )  # Inform the player that the dragon won the battle
                 break  # Break out of the loop to end the battle
-            
+
     elif answer == "2":  # Check if the user chose elf
         character = elf  # Set the character variable to elf
         # Start a loop for the elf's battle with the dragon
@@ -77,3 +77,24 @@ while True:
                     "The dragon has defeated you!"
                 )  # Inform the player that the dragon won the battle
                 break  # Break out of the loop to end the battle
+
+    elif answer == "3":  # Check if the user chose human
+        character = human  # Set the character variable to human
+        # Start a loop for the human's battle with the dragon
+        while (
+            human_hp > 0 and dragon_hp > 0
+        ):  # Continue the loop while both human and dragon are alive
+            # Human attacks first
+            dragon_hp -= human_damage  # Subtract human's damage from dragon's health
+            print(
+                f"{character} attacked dragon for {human_damage} damage."
+            )  # Display message indicating human's attack
+
+            if dragon_hp <= 0:  # Check if dragon is defeated after human's attack
+                print(
+                    "You have defeated the dragon!"
+                )  # Congratulate the player for defeating the dragon
+                break  # Break out of the loop to end the battle
+
+            # Dragon attacks back
+            human_hp
