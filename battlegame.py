094244b3e087c8aa8a86c0d1703a520a89987608
Skylar-Lorena <lorayne911@gmail.com>
2023-11-28 15:20:17 +0300

    check if user chose human

--- battlegame.py
+++ battlegame.py
@@ -78,23 +78,11 @@ while True:
                 )  # Inform the player that the dragon won the battle
                 break  # Break out of the loop to end the battle
 
-    elif answer == "3":  # Check if the user chose human
-        character = human  # Set the character variable to human
-        # Start a loop for the human's battle with the dragon
-        while (
-            human_hp > 0 and dragon_hp > 0
-        ):  # Continue the loop while both human and dragon are alive
-            # Human attacks first
-            dragon_hp -= human_damage  # Subtract human's damage from dragon's health
-            print(
-                f"{character} attacked dragon for {human_damage} damage."
-            )  # Display message indicating human's attack
+    # Check if the user chose human
+    elif answer == "3":
+        character = human
 
-            if dragon_hp <= 0:  # Check if dragon is defeated after human's attack
-                print(
-                    "You have defeated the dragon!"
-                )  # Congratulate the player for defeating the dragon
-                break  # Break out of the loop to end the battle
+        my_hp = human_hp
+        my_damage = human_damage
 
-            # Dragon attacks back
-            human_hp
+        break
