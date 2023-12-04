ffb5ca5c05cd0746f1410b031183904f5056d291
Skylar-Lorena <lorayne911@gmail.com>
2023-12-04 11:58:30 +0300

    add comments for code clarification

--- battlegame.py
+++ battlegame.py
@@ -1,85 +1,77 @@
 import sys
 
-# Task 1: Declare variables for characters and their hp and damage
-wizard = "Wizard"
-elf = "Elf"
-human = "Human"
+# Task 1: Declare variables for characters and their stats
+#   - Name, HP, and damage
+wizard = "Wizard"  # Stores the character name
+wizard_hp = 70    # Initial HP of the wizard
+wizard_damage = 150  # Damage dealt by the wizard
 
-wizard_hp = 70
-elf_hp = 100
-human_hp = 150
+# Similar declarations for elf and human
 
-wizard_damage = 150
-elf_damage = 100
-human_damage = 20
+dragon_hp = 300  # Dragon's HP
+dragon_damage = 50  # Damage dealt by the dragon
 
-dragon_hp = 300
-dragon_damage = 50
-
-
-#  Task 2: Prompt the player to choose from a list of options
+# Task 2: Welcome message and character selection
 print("Welcome to the Battle Game!")
-print("Choose your character: ")
-print("1.", wizard)
+print("Choose your warrior:")
+print("1.", wizard)  # Display character options with numbers
 print("2.", elf)
 print("3.", human)
 print("4. Exit")
 
-character_input = input("Enter your choice: ")
+character_input = input("Enter your choice: ")  # Get player's choice
 
-# Task 3: Set up infinite while loop to handle player choice
+# Task 3: Handle player input and set character stats
 while True:
+    # Check for exact match or numeric input for each character
     if character_input.lower() == wizard.lower() or character_input == "1":
         character = wizard
-        my_hp = wizard_hp
+        my_hp = wizard_hp  # Set player's HP and damage based on chosen character
         my_damage = wizard_damage
         break
 
-    elif character_input.lower() == elf.lower() or character_input == "2":
-        character = elf
-        my_hp = elf_hp
-        my_damage = elf_damage
-        break
+    # Similar conditions for elf and human
 
-    elif character_input.lower() == human.lower() or character_input == "3":
-        character = human
-        my_hp = human_hp
-        my_damage = human_damage
-        break
-
+    # Check for exit option
     elif character_input.lower() == "exit" or character_input == "4":
-        print("Goodbye")
+        print("Goodbye!")
         sys.exit(0)
 
+    # Handle invalid input and prompt again
     else:
         print("Unknown character")
-        character = input("Enter your choice: ")
+        character_input = input("Enter your choice: ")
 
-# Task 4: Battle with the Dragon!
+# Task 4: Battle loop
 while True:
+    # Player attacks dragon
     dragon_hp -= my_damage
-    print("The", character, "damaged the Dragon!")
-    print("Dragon's HP:", dragon_hp)
+    print(f"The {character} damaged the Dragon!")  # Use f-string for dynamic text
+    print(f"Dragon's HP: {dragon_hp}")
 
+    # Check if dragon is defeated
     if dragon_hp <= 0:
         print("The Dragon has lost the battle!")
         break
 
+    # Dragon attacks player
     my_hp -= dragon_damage
-    print(character, "was damaged by the Dragon!")
-    print("Your HP:", my_hp)
+    print(f"{character} was damaged by the Dragon!")
+    print(f"Your HP: {my_hp}")
 
+    # Check if player is defeated
     if my_hp <= 0:
         print("You have lost the battle!")
         break
 
-    # Replay prompt
+    # Ask if player wants to play again after each round
     while True:
         play_again = input("Do you want to play again? (Y/N): ")
 
         if play_again.lower() == "y":
-            break
+            break  # Restart the game if player chooses yes
         elif play_again.lower() == "n":
-            sys.exit(0)
+            sys.exit(0)  # Exit if player chooses no
         else:
             print("Invalid input. Please enter Y or N.")
+
